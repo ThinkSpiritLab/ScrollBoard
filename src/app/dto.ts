32346d3c64abf8c96698bdef7c96118c3bb44c6b
Nugine <nugine@foxmail.com>
2020-07-30 01:37:24 +0800

    add dto

---
+++ src/app/dto.ts
@@ -0,0 +1,28 @@
+export interface Contest {
+    problems: Problem[];
+    users: User[];
+    submissions: Submission[];
+    duration: number;
+    penaltyTime: number;
+    freezeTime: number;
+    name: string;
+}
+
+export interface Problem {
+    id: string;
+    tag: string;
+    color?: string;
+}
+
+export interface User {
+    id: string;
+    name: string;
+}
+
+export interface Submission {
+    id: string;
+    userId: string;
+    problemId: string;
+    submitTime: number;
+    accepted: boolean;
+}
