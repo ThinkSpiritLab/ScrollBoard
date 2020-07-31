8ac8e219fa49f40839d6327b824464b13416ae08
Nugine <nugine@foxmail.com>
2020-08-01 00:25:50 +0800

    disable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,9 @@ import * as serviceWorker from "./serviceWorker";
 import App from "./app/App";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    // <React.StrictMode>
+    <App />,
+    // </React.StrictMode>,
     document.getElementById("root")
 );
 
