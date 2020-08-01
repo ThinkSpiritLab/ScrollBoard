beb6d06f725dae0229f111dcf25c4f7b5a6488fd
Nugine <nugine@foxmail.com>
2020-08-01 17:54:26 +0000

    Deploying to gh-pages from  @ 8ee0a252d079330bc3c761a5657c1736c11422c0 🚀

---
+++ precache-manifest.88368919903d665d0c24f5e02c8b897a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bd31b13213fbf548b4c2a09c620bcb",
+    "url": "/ScrollBoard/index.html"
+  },
+  {
+    "revision": "8dbb179f0ddd76ec051c",
+    "url": "/ScrollBoard/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "5728380f9e579d8504b2",
+    "url": "/ScrollBoard/static/css/main.651be2ac.chunk.css"
+  },
+  {
+    "revision": "8dbb179f0ddd76ec051c",
+    "url": "/ScrollBoard/static/js/2.51141c89.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ScrollBoard/static/js/2.51141c89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5728380f9e579d8504b2",
+    "url": "/ScrollBoard/static/js/main.8bce092c.chunk.js"
+  },
+  {
+    "revision": "48f2c97b95d78083ecd2",
+    "url": "/ScrollBoard/static/js/runtime-main.9636b994.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ScrollBoard/precache-manifest.8636a02ba5e049fb657d5fd4a9d3cd90.js"
+  "/ScrollBoard/precache-manifest.88368919903d665d0c24f5e02c8b897a.js"
 );
 
 self.addEventListener('message', (event) => {
