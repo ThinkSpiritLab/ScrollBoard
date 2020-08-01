bab140fff2f52df8366d25a2ae67329799219de0
Nugine <nugine@foxmail.com>
2020-08-01 15:48:18 +0800

    dev2 demo

--- README.md
+++
@@ -1,6 +0,0 @@
-# ScrollBoard
-
-> 滚榜动画
-
-[使用方法：README.react.md](README.react.md)
-
---
+++ precache-manifest.3a4efe3ebfdaf228cc78db333a2c88b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6a6784b8ae3c62c38149eb5d540fed",
+    "url": "/ScrollBoard/index.html"
+  },
+  {
+    "revision": "59dbaf3ef648dc2eac83",
+    "url": "/ScrollBoard/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "c976483c599a05d6209c",
+    "url": "/ScrollBoard/static/css/main.aac6a13b.chunk.css"
+  },
+  {
+    "revision": "59dbaf3ef648dc2eac83",
+    "url": "/ScrollBoard/static/js/2.27f7f651.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ScrollBoard/static/js/2.27f7f651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c976483c599a05d6209c",
+    "url": "/ScrollBoard/static/js/main.ffd8fcc4.chunk.js"
+  },
+  {
+    "revision": "48f2c97b95d78083ecd2",
+    "url": "/ScrollBoard/static/js/runtime-main.9636b994.js"
+  }
+]);
